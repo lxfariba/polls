4755ca8fcf74363c90a59585a790e002866e0fd4
lxfariba <f.karvand@gmail.com>
2016-11-19 20:19:08 +0330

    views and urls and tests and ...

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+	model = Choice
+	extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+	feildsets = [
+	(None,             {'feilds':['question_text']}),
+	('Dateinformation', {'feilds':['pub_date'], 'classes':['collapse']}),
+	]
+	inlines = [ChoiceInline]
+
+	list_display = ('question_text','pub_date','was_published_recently')
+	list_filter = ['pub_date']
+	search_feilds = ['question_text']
+	
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.utils import timezone 
+from .models import Question 
+from django.urls import revers
 
-# Create your tests here.
+
+import datetime
+
+
+class QuestionMethodTests(TestCase):
+
+	def test_was_published_recently_with_old_question(self):
+		time = timezone.now() -  datetime.timedelta(days=30)
+		old_question = Question(pub_date=time)
+		self.assertIs(old_question.was_published_recently(), False)
+
+
+	def test_was_published_recently_with_recent_question(self):
+		time = timezone.now() - datetime.timedelta(hours = 1)
+		recent_question = Question(pub_date=time)
+		self.assertIs(recent_question.was_published_recently(), True)	
+
+
+	def create_question (question_text, days):
+	
+		time = timezone.now() + datetime.timedelta(days=days)
+		return Question.objects.create(question_text=question_text, pub_date=time)	
